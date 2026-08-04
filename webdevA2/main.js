c6f16a4ef81a8a416f3be77757f0a30da5bdb4b0
doomedforlife <kaixinkhoo72@gmail.com>
2026-08-05 06:48:44 +0800

    wow all nighter bruh

--- webdevA2/main.js
+++ webdevA2/main.js
@@ -43,3 +43,70 @@ hamBtn.innerHTML="Close Menu"; //change button text to chose menu
 hamBtn.innerHTML="Open Menu"; //change button text open menu
 }
 }
+
+//fullscreen
+const btnFS=document.querySelector("#btnFS");
+const btnWS=document.querySelector("#btnWS");
+btnFS.addEventListener("click",enterFullscreen);
+btnWS.addEventListener("click",exitFullscreen);
+function enterFullscreen() 
+{ //must be called by user generated event
+	if (document.documentElement.requestFullscreen) 
+	{
+		document.documentElement.requestFullscreen();
+	} 
+	else if (document.documentElement.mozRequestFullScreen) 
+	{ // Firefox
+		document.documentElement.mozRequestFullScreen();
+	} 
+	else if (document.documentElement.webkitRequestFullscreen) 
+	{ // Chrome, Safari, and Opera
+		document.documentElement.webkitRequestFullscreen();
+	} 
+	else if (document.documentElement.msRequestFullscreen) 
+	{ // IE/Edge
+		document.documentElement.msRequestFullscreen();
+	}
+}
+function exitFullscreen() 
+{
+	if (document.exitFullscreen) 
+	{
+		document.exitFullscreen();
+	} 
+	else if (document.mozCancelFullScreen) 
+	{ // Firefox
+		document.mozCancelFullScreen();
+	} 
+	else if (document.webkitExitFullscreen) 
+	{ // Chrome, Safari, and Opera
+		document.webkitExitFullscreen();
+	} 
+	else if (document.msExitFullscreen) 
+	{ // IE/Edge
+		document.msExitFullscreen();
+	}
+}
+
+//game
+const coin = document.getElementById("coin");
+function GetRandom(min,max){
+//this will select a number between min and max
+return Math.round(Math.random() * (max - min)) + min;
+}
+function MoveCoin() {
+coin.style.left = GetRandom(0, 500) + "vw";
+coin.style.top = GetRandom(0, 500) + "vh";
+}
+var moveCoinItvId = setInterval(MoveCoin, 1000);
+
+const scoreBox=document.getElementById("scoreBox");
+var score=0; //to track how many clicks
+function coinCatch() {
+//increases score after clicking
+score++;
+//update html scorebox
+scoreBox.innerHTML = "Score: " + score;
+}
+//link coin to mouseclick to coinCatch function
+coin.addEventListener("click",coinCatch);
